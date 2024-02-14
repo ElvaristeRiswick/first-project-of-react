08449071fe3874fa5e5dce8a08cdb05397cd2c2c
Riswick <riswickelvariste@gmail.com>
2024-02-14 11:15:30 -0400

    first project of react

---
+++ devoirReact/src/App.jsx
@@ -0,0 +1,11 @@
+import { StudentsCard } from "./components/StudentsCard";
+
+const App = () => {
+  return (
+    <>
+      <StudentsCard Nom="DJIMELOVE" age={22} pays="Rp dominicaine" />
+    </>
+  );
+};
+
+export default App;
---
+++ devoirReact/src/components/StudentsCard/index.jsx
@@ -0,0 +1,9 @@
+export const StudentsCard = ({ Nom, age, pays }) => {
+  return (
+    <>
+      <h1>{Nom}</h1>
+      <h2>{age}</h2>
+      <h3>{pays} </h3>
+    </>
+  );
+};
